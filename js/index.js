f99ec90444e7eb02c72a575d063c997d51d7e353
cvanduyne <35565350+cvanduyne@users.noreply.github.com>
2018-01-21 10:04:34 -0500

    Fixed logic for back camera

--- js/index.js
+++ js/index.js
@@ -12,14 +12,14 @@ scanner.addListener('scan', function (content) {
 Instascan.Camera.getCameras().then(function (cameras) {
   if (cameras.length > 0) {
     var c = cameras[0]; // Default to first
-    for (var i=1; i < cameras.length; i++)
+    for (var i=0; i < cameras.length; i++)
     {
       // Prefer any camera that mentions "back"
-      if (cameras[i].name.toLowerCase().contains("back"))
+      if (cameras[i].name.toLowerCase().includes("back"))
       {
         c = cameras[i];
         break;
-      }  
+      }
     }
     scanner.start(c);
   } else {
