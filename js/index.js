45e62f3c3e683934c014ecbf3d02c3a95d6e3b2a
cvanduyne <35565350+cvanduyne@users.noreply.github.com>
2018-01-21 18:48:40 -0500

    deferring load of cameras

--- js/index.js
+++ js/index.js
@@ -9,34 +9,37 @@ scanner.addListener('scan', function (content) {
     gameLogic.HandleScan(content);
   }
 });
-Instascan.Camera.getCameras().then(function (cameras) {
-  if (cameras.length > 0) {
-    var c = cameras[0]; // Default to first
-    for (var i=0; i < cameras.length; i++)
-    {
-      // Prefer any camera that mentions "back"
-      try {
-        var debug = document.getElementById('debug');
-        if (debug){
-          debug.innerHTML = debug.innerHTML + cameras[i].name + "</br>";
-        }
-        if (cameras[i].name.toLowerCase().includes("back"))
-        {
-          c = cameras[i];
-          break;
-        }
-      }
-      catch(e)
+
+window.addEventListener('load', function() {
+  Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+      var c = cameras[0]; // Default to first
+      for (var i=0; i < cameras.length; i++)
       {
-          debug.innerHTML = debug.innerHTML + "got error: "+e+ "</br>";
+        // Prefer any camera that mentions "back"
+        try {
+          var debug = document.getElementById('debug');
+          if (debug){
+            debug.innerHTML = debug.innerHTML + cameras[i].name + "</br>";
+          }
+          if (cameras[i].name.toLowerCase().includes("back"))
+          {
+            c = cameras[i];
+            break;
+          }
+        }
+        catch(e)
+        {
+            debug.innerHTML = debug.innerHTML + "got error: "+e+ "</br>";
+        }
       }
+      debug.innerHTML = debug.innerHTML + "selected: " +c.name + "</br>";
+      scanner.start(c);
+    } else {
+      console.error('No cameras found.');
     }
-    debug.innerHTML = debug.innerHTML + "selected: " +c.name + "</br>";
-    scanner.start(c);
-  } else {
-    console.error('No cameras found.');
-  }
-}).catch(function (e) {
-  console.error(e);
-  debug.innerHTML = debug.innerHTML + "error:" + e
+  }).catch(function (e) {
+    console.error(e);
+    debug.innerHTML = debug.innerHTML + "error:" + e
+  });
 });
