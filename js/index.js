4e58b2616aafbe3f3602c903cec75662ad823dc8
cvanduyne <35565350+cvanduyne@users.noreply.github.com>
2018-01-23 18:22:33 -0500

    Cleaning up debug

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 
 "use strict";
 
+var debugElement = document.getElementById('debug');
+var originalConsoleLog = console.log;
 console.log = function (message) {
-  var debug = document.getElementById('debug');
-  if (debug){
-              debug.innerHTML = debug.innerHTML + message + "</br>";
-            }
-            
+  if (debugElement)
+  {
+    debugElement.innerHTML = debug.innerHTML + message + "</br>";
+  }
+  originalConsoleLog(message);
 }
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
 scanner.addListener('scan', function (content) {
@@ -21,29 +23,25 @@ window.addEventListener('load', function() {
   Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
       var c = cameras[0]; // Default to first
-      /*
-      for (var i=0; i < cameras.length; i++)
-      {
-        // Prefer any camera that mentions "back"
-        try {
-          var debug = document.getElementById('debug');
-          if (debug){
-            debug.innerHTML = debug.innerHTML + cameras[i].name + "</br>";
-          }
+      try {
+        for (var i=0; i < cameras.length; i++)
+        {
+          // Prefer any camera that mentions "back"
+          console.log(cameras[i].name);
           if (cameras[i].name.toLowerCase().includes("back"))
           {
             c = cameras[i];
             break;
           }
         }
-        catch(e)
-        {
-            debug.innerHTML = debug.innerHTML + "got error: "+e+ "</br>";
-        }
+        console.log( "selected: "+c.name);
+
+        scanner.start(c);
       }
-      debug.innerHTML = debug.innerHTML + "selected: " +c.name + "</br>";
-      */
-      scanner.start(c);
+      catch(e)
+      {
+          console.log( "got error: "+e + " skipping start.")
+      }
     } else {
       console.error('No cameras found.');
     }
